ea871f29b8ca7792512bae2f3ddd99f23c53a1d4
ligoujiang <2571373878@qq.com>
2024-04-09 23:16:43 +0800

    2024/4/9 23:16

--- Demo04.cpp
+++ Demo04.cpp
@@ -3,9 +3,10 @@ extern "C" {
 #include <libavutil/avutil.h>
 #include <libavcodec/avcodec.h>
 }
-//转封装（不解码）
+#include <iostream>
+//mp4转flv格式转封装（不解码）
 int main(int argc,char** argv){
-    if(argc<2){
+    if(argc<3){
         av_log(NULL,AV_LOG_ERROR,"%s infileName.\n",argv[0]);
         return -1;
     }
@@ -32,7 +33,9 @@ int main(int argc,char** argv){
         return -1;
         
     }
-    //创建输出流
+    av_dump_format(inCtx,0,infileName,0);   //打印输入文件细节
+
+    //创建输出流句柄
     AVFormatContext *outCtx=NULL;
     ret=avformat_alloc_output_context2(&outCtx,NULL,NULL,outfileName);
     if(ret<0){
@@ -59,21 +62,90 @@ int main(int argc,char** argv){
     int streamIndex=0;
     for(int i=0;i<streamCount;i++){
         AVStream *inStream=inCtx->streams[i];
-        if(inStream->codecpar->codec_type!=AVMEDIA_TYPE_VIDEO&&
+        if(inStream->codecpar->codec_type!=AVMEDIA_TYPE_VIDEO&& //当不是视频流 音频流或字幕流时，不处理
             inStream->codecpar->codec_type!=AVMEDIA_TYPE_AUDIO&&
             inStream->codecpar->codec_type!=AVMEDIA_TYPE_SUBTITLE){
                 handleStreamIndexArray[i]=-1;
                 continue;
         }
+        //创建输出流
         handleStreamIndexArray[i]=streamIndex++;
-        
-
+        AVStream *outStream=avformat_new_stream(outCtx,NULL);
+        if(outStream==NULL){
+            av_log(NULL,AV_LOG_ERROR,"create outStream failed!\n");
+            avformat_close_input(&inCtx);
+            avformat_free_context(outCtx);
+            return -1;
+        }
+        //拷贝源音视频流编码参数
+        if(avcodec_parameters_copy(outStream->codecpar,inStream->codecpar)<0){
+            av_log(NULL,AV_LOG_ERROR,"copy outStream failed!\n");
+            avformat_close_input(&inCtx);
+            avformat_free_context(outCtx);
+            return -1;
+        }
+        outStream->codecpar->codec_tag=0;
     }
 
+    //打开输出文件
+        if(!(outCtx->oformat->flags&AVFMT_NOFILE)){
+            ret=avio_open(&outCtx->pb,outfileName,AVIO_FLAG_WRITE);
+            if(ret<0){
+                av_log(NULL,AV_LOG_ERROR,"avio_open failed!\n");
+                avformat_close_input(&inCtx);
+                avformat_free_context(outCtx);
+                return -1;
+            }
+        }
+        ret=avformat_write_header(outCtx,NULL);
+        if(ret<0){
+                av_log(NULL,AV_LOG_ERROR,"avio_open failed!\n");
+                avformat_close_input(&inCtx);
+                avformat_free_context(outCtx);
+                return -1;
+        }
 
-fail:
+        AVPacket *pkt=av_packet_alloc();
+        while(av_read_frame(inCtx,pkt)==0){
+            if(pkt->stream_index>=streamCount||handleStreamIndexArray[pkt->stream_index]==-1){
+                av_packet_unref(pkt);
+                continue;
+            }
+            AVStream *inStream=inCtx->streams[pkt->stream_index];
+            AVStream *outStream=outCtx->streams[pkt->stream_index];
+            pkt->stream_index=handleStreamIndexArray[pkt->stream_index];
+
+            //时间基转换
+            pkt->pts=av_rescale_q(pkt->pts,inStream->time_base,outStream->time_base);
+            pkt->dts=av_rescale_q(pkt->dts,inStream->time_base,outStream->time_base);
+            pkt->duration=av_rescale_q(pkt->duration,inStream->time_base,outStream->time_base);
+            pkt->pos=-1;
+
+            //写入输出文件
+            ret=av_interleaved_write_frame(outCtx,pkt);
+            if(ret!=0){
+                av_log(NULL,AV_LOG_ERROR,"avio_open failed!\n");
+                avformat_close_input(&inCtx);
+                avio_closep(&outCtx->pb);
+                avformat_free_context(outCtx);
+                return -1;
+            }
+            av_packet_unref(pkt);
+        }
+        ret=av_write_trailer(outCtx);
+        if(ret!=0){
+                av_log(NULL,AV_LOG_ERROR,"avio_open failed!\n");
+                avformat_close_input(&inCtx);
+                avio_closep(&outCtx->pb);
+                avformat_free_context(outCtx);
+                return -1;
+            }
+    avio_closep(&outCtx->pb);
     if(inCtx){
         avformat_close_input(&inCtx);
+    }
+    if(outCtx){
+        avformat_free_context(outCtx);
     }
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,10 +1,198 @@
 #include <iostream>
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libswresample/swresample.h>
+#include <libavcodec/bsf.h>
+}
+ 
 using namespace std;
-int main(){
-    int *arry=new int[5];
-    arry[4]=6;
-    for(int i=0;i<5;i++){
-        cout<<arry[i]<<endl;
+ 
+int main(int argc,char**argv)
+{
+    if(argc != 3)
+    {
+        printf("invalid parameters\n");
+        return -1;
     }
+ 
+    av_log_set_level(AV_LOG_DEBUG);
+ 
+    char input[64] = {0},
+         output[64] = {0};
+ 
+    strncpy(input,argv[1],sizeof(input)-1);
+    strncpy(output,argv[2],sizeof(output)-1);
+    av_log(nullptr,AV_LOG_DEBUG,"input:%s output:%s\n",input,output);
+ 
+    AVFormatContext *ifmt_ctx = nullptr,
+                    *ofmt_ctx = nullptr;
+    int ret = 0;
+ 
+    if((ret = avformat_open_input(&ifmt_ctx,input,0,0)) < 0)
+    {
+        av_log(nullptr,AV_LOG_ERROR,"Could not find open input file.\n");
+        return -1;
+    }
+ 
+    if((ret = avformat_find_stream_info(ifmt_ctx,0)) < 0 )
+    {
+        av_log(nullptr,AV_LOG_ERROR,"Could not find stream.\n");
+        return -1;
+    }
+    av_log(nullptr,AV_LOG_DEBUG,"input name:%s longname:%s\n",ifmt_ctx->iformat->name,ifmt_ctx->iformat->long_name);
+ 
+    av_dump_format(ifmt_ctx,0,input,0);   //打印输入文件细节
+    
+    av_log(nullptr,AV_LOG_DEBUG,"duration=\n");
+    av_log(nullptr,AV_LOG_DEBUG,"filename=%s\n",ifmt_ctx->url);
+    if((ret = avformat_alloc_output_context2(&ofmt_ctx,nullptr,nullptr,output)) < 0)
+    {
+        av_log(nullptr,AV_LOG_ERROR,"Could not alloc output context.\n");
+        return -1;
+    }
+    bool isBsf = false; //是否转换h264模式
+    if(!strncmp(ofmt_ctx->oformat->name,"avi",strlen("avi")))
+        isBsf = true;
+	
+    av_log(nullptr,AV_LOG_DEBUG,"output name:%s longname:%s\n",ofmt_ctx->oformat->name,ofmt_ctx->oformat->long_name);
+ 
+    /*必须申请一个内存，否则后续各种段错误，ffmpeg存在很多结果初始化需要分配内存*/
+    AVCodecParameters *par = avcodec_parameters_alloc();
+    for(int i = 0;i < ifmt_ctx->nb_streams;i++)
+    {
+        AVStream *in_stream = ifmt_ctx->streams[i];
+        const AVCodec *codec = avcodec_find_encoder(in_stream->codecpar->codec_id);
+        AVStream *out_stream = avformat_new_stream(ofmt_ctx,codec);
+        if(!out_stream)
+        {
+            av_log(nullptr,AV_LOG_ERROR,"Failed allocating output Stream.");
+            return -1;
+        }
+        ret = avcodec_parameters_copy(out_stream->codecpar, in_stream->codecpar);
+        if(ret < 0)
+        {
+            av_log(nullptr,AV_LOG_ERROR,"Failed paste context to output.");
+            return -1;
+        }
+ 
+        av_log(nullptr,AV_LOG_DEBUG,"codec_tag=%d\n",out_stream->codecpar->codec_tag);
+        /*ffmpeg中，avformat_write_header 调用init_muxer时会判断par->codec_tag，如果不为0，会进行附加验证*/
+        out_stream->codecpar->codec_tag = 0;
+        if(in_stream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+            ret = avcodec_parameters_copy(par, in_stream->codecpar);
+            if(ret < 0)
+            {
+                av_log(nullptr,AV_LOG_ERROR,"Failed paste context to par.");
+                return -1;
+            }
+        }
+    }
+    av_dump_format(ofmt_ctx,0,output,1);
+    if (!(ofmt_ctx->oformat->flags & AVFMT_NOFILE))
+    {
+        ret = avio_open(&ofmt_ctx->pb, output, AVIO_FLAG_WRITE);
+        if (ret < 0)
+        {
+            printf("Could not open output file %s ", output);
+            return -1;
+        }
+    }
+ 
+    /*AVPacket是存储压缩编码数据相关信息的结构体。 保存的是解码前的数据，也就是压缩后的数据。 
+    该结构体不直接包含数据，其中有一个执行数据域的指针，ffmpeg中的很多数据结构都是用这种方法来管理
+    它保存了解复用之后，解码之前的数据（仍然是压缩后的数据）和关于这些数据的一些附加信息，如
+    显示时间戳（pts）、解码时间戳（dts）、数据时长，所在媒体流的索引等*/
+    AVPacket *pkt = av_packet_alloc();
+    AVBSFContext *bsf_ctx = nullptr;
+    if(isBsf)
+    {
+        const AVBitStreamFilter *pfilter = av_bsf_get_by_name("h264_mp4toannexb");
+        av_bsf_alloc(pfilter, &bsf_ctx);
+        bsf_ctx->par_in = par;
+        av_bsf_init(bsf_ctx);
+    }
+    ret = avformat_write_header(ofmt_ctx, nullptr);
+    int frames = 0;
+    int frame_index = 0;
+    while(1)
+    {
+        AVStream *in_stream, *out_stream;
+        ret = av_read_frame(ifmt_ctx,pkt);
+        if(ret < 0)
+        {
+            av_log(nullptr,AV_LOG_INFO,"read frame from ifmt.\n");
+            break;
+        }
+        in_stream = ifmt_ctx->streams[pkt->stream_index];
+        out_stream = ofmt_ctx->streams[pkt->stream_index];
+        /*实际上time_base的意思就是时间的刻度：
+         * 如（1,25），那么时间刻度就是1/25 （1,9000），那么时间刻度就是1/90000 那么，在刻度为1/25的体系下的time=5，转换成在刻度为1/90000体系下的时间time为(5*1/25) / (1/90000) = 3600*5 = 18000，ffmpeg中作pts计算时，存在大量这种转换。*/
+        /*如果没有显示时间戳自己加上时间戳并且将显示时间戳赋值给解码时间戳*/
+        if(pkt->pts==AV_NOPTS_VALUE)
+        {
+            av_log(nullptr,AV_LOG_DEBUG,"AV_NOPTS_VALUE\n");
+            //Write PTS
+            AVRational time_base1=in_stream->time_base;
+            //Duration between 2 frames (us)
+            int64_t calc_duration=(double)AV_TIME_BASE/av_q2d(in_stream->r_frame_rate);
+            //Parameters
+            pkt->pts=(double)(frame_index*calc_duration)/(double)(av_q2d(time_base1)*AV_TIME_BASE);
+            pkt->dts=pkt->pts;
+            pkt->duration=(double)calc_duration/(double)(av_q2d(time_base1)*AV_TIME_BASE);
+            frame_index++;
+        }
+        pkt->pts = av_rescale_q_rnd(pkt->pts,in_stream->time_base,out_stream->time_base,static_cast<AVRounding>(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+        pkt->dts = av_rescale_q_rnd(pkt->dts,in_stream->time_base,out_stream->time_base,static_cast<AVRounding>(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+        pkt->duration = av_rescale_q(pkt->duration,in_stream->time_base,out_stream->time_base);
+        pkt->pos = -1;   //< byte position in stream, -1 if unknown
+ 
+        if(pkt->stream_index == 0 && isBsf)  //video
+        {
+            AVPacket *fpkt = av_packet_clone(pkt);            
+            av_bsf_send_packet(bsf_ctx, pkt);
+            while(av_bsf_receive_packet(bsf_ctx, fpkt) == 0)
+            {
+                if(av_interleaved_write_frame(ofmt_ctx,fpkt) < 0)
+                {
+                    av_log(nullptr,AV_LOG_ERROR,"Error muxing packed");
+                    break;
+                }
+            }
+ 
+            av_packet_unref(fpkt);
+        }
+        else
+        {
+            ret = av_interleaved_write_frame(ofmt_ctx, pkt);
+            if(ret < 0)
+            {
+                av_log(nullptr,AV_LOG_ERROR,"Error muxing packed");
+                break;
+            }
+        }
+ 
+        av_packet_unref(pkt);
+    }
+    av_write_trailer(ofmt_ctx);
+    avformat_free_context(ifmt_ctx);
+    if(par)
+    {
+        avcodec_parameters_free(&par);
+        if(bsf_ctx)
+            bsf_ctx->par_in = nullptr;
+    }
+    if(bsf_ctx)
+        av_bsf_free(&bsf_ctx);
+ 
+    if(ofmt_ctx && !(ofmt_ctx->oformat->flags & AVFMT_NOFILE))
+    {
+        avio_close(ofmt_ctx->pb);
+    }
+    avformat_free_context(ofmt_ctx);
+ 
     return 0;
 }
